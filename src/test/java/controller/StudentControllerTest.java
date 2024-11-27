a0583270db270b24b67e5c46a6c52b32d5492aa7
Nguyen Su Tat <dat.dnst@byteuptech.com>
2024-11-27 14:49:54 +0700

    demo stub

--- src/test/java/controller/StudentControllerTest.java
+++ src/test/java/controller/StudentControllerTest.java
@@ -18,24 +18,14 @@ public class StudentControllerTest {
     private StudentController studentController;
 
     @Test
-    void test_findStudentByIdEq1 () {
+    void test_findStudentByIdEq1() {
         studentController = new StudentController();
-        StudentDao mockStudentDao = Mockito.mock(StudentDao.class);
-        Student mockStudent = new Student(1, "A", 1);
-        when(mockStudentDao.findById(1)).thenReturn(mockStudent);
-        studentController.setStudentDao(mockStudentDao);
+        studentController.setStudentDao(new StudentDaoImplStub());
         var student = studentController.findByid(1);
         Assertions.assertNotNull(student);
-        Assertions.assertEquals(student.getId(),1);
-    }
-
-
-    @Test
-    void test_findStudentByIdEq2 () {
-        studentController = new StudentController();
-        var verifyStudent = new Student(2, "B", 2);
-        var actualStudent = studentController.findByid(verifyStudent.getId());
-        Assertions.assertNotNull(verifyStudent);
-        Assertions.assertEquals(actualStudent.getId(),2);
+        Assertions.assertEquals(student.getId(), 1);
     }
 }
+
+
+
--- src/test/java/dao/StudentDaoTest.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import org.junit.jupiter.api.Test;
-
-public class StudentDaoTest {
-
-    @Test
-    public void test() {}
-}
